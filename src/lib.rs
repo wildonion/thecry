5b8ff020227283ce5bd448dfdd25d6259e71efe7
wildonion <ea_pain@yahoo.com>
2023-08-29 11:41:55 +0330

    add zkp module

--- src/lib.rs
+++ src/lib.rs
@@ -235,3 +235,22 @@ pub mod utils{
     }
     
 }
+
+
+pub mod zkp{
+
+    pub use super::*;
+
+    pub struct ZkpError;
+    pub struct Verifier;
+    pub struct Prover;
+
+    // pub async fn auth(wallet: &mut Wallet) -> Result<(), ZkpError>{
+
+    //     Ok(())
+    // }
+
+    // https://noir-lang.org/index.html
+    // https://github.com/rust-cc/awesome-cryptography-rust#zero-knowledge-proofs
+
+}
